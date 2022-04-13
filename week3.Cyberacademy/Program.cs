756326e94491307a4e588c5414177c5978a9207d
H0me1 <H0me1@OBAFEMI>
2022-04-13 11:01:46 +0100

    Add project files.

---
+++ week3.Cyberacademy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace week3.Cyberacademy
+{
+    public delegate int MyDelegate(int i);
+    public class Program
+    {
+        static void Main()
+        {
+
+            MyDelegate del = MyMethod;
+            del += MyMethod2;
+            del(89);
+            //People p = new People();
+
+            //People.Person person = p[1];
+            //Console.WriteLine(person.Name);
+        }
+        static void MyMethod(int i)
+        {
+            Console.WriteLine("MyMethod {0}", i);
+        }
+        static void MyMethod2(int i)
+        {
+            Console.WriteLine("MyMethod2 {0}", i);
+        }
+    }
+}
+
+//public class People
+//{
+//    public class Person
+//    {
+//        public string Name { get; set; }
+//}
+//private readonly Person[] names =
+//    {
+//        new Person{ Name = "Bill gate"},
+//        new Person{ Name = "Prolifik lexzy" },
+//        new Person{ Name = "Scott Allen"},
+//        new Person{ Name = " Damilare Obafemi"}
+//    };
+
+//    public Person this[int position]
+//    {
+//        get
+//        {
+//            return names[position];
+//        }
+//    }
+//}
+
+
